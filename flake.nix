0779b3877f044585c1407392257991190bd680f7
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-13 07:31:59 +0530

    feat(programs): install zen browser

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
-    inputs@{ self, nixpkgs, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      zen-browser,
+      ...
+    }:
     {
       nixosConfigurations.tesseract = nixpkgs.lib.nixosSystem {
+
+        specialArgs = {
+          inherit inputs;
+        };
+
         modules = [ ./configuration.nix ];
       };
     };
