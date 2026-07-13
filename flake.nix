6292b3c182c9cd08bd105489393b04c2fe87751b
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-13 06:35:43 +0530

    style: format nix configuration files

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.tesseract = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
+      nixosConfigurations.tesseract = nixpkgs.lib.nixosSystem {
+        modules = [ ./configuration.nix ];
+      };
     };
-  };
 }
-
