8e28dc0a76328ba8247156beaa361d4c28885093
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-17 10:18:22 +0530

    style(theme): update system color scheme

--- configuration.nix
+++ configuration.nix
@@ -162,7 +162,7 @@
   stylix = {
     enable = true;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-moon.yaml";
 
     polarity = "dark";
 
