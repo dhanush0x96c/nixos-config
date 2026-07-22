eb725eec7b9cea4838d6edadcadb84f9e64d3ab8
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-22 10:51:51 +0530

    refactor(theme): extract stylix setup into standalone module

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.default
     ./programs/kanata
+    ./programs/stylix
   ];
 
   # Bootloader.
@@ -164,41 +165,6 @@
     };
   };
 
-  stylix = {
-    enable = true;
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-moon.yaml";
-
-    polarity = "dark";
-
-    fonts = {
-      sansSerif = {
-        package = pkgs.inter;
-        name = "Inter";
-      };
-
-      serif = {
-        package = pkgs.inter;
-        name = "Inter";
-      };
-
-      monospace = {
-        package = pkgs.jetbrains-mono;
-        name = "JetBrains Mono";
-      };
-
-      emoji = {
-        package = pkgs.noto-fonts-color-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
-
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
-    };
-  };
   # ---------------------------------------------------
 
   # Some programs need SUID wrappers, can be configured further or are
