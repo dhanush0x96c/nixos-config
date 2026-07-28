9180707fc4ab0885f063ffcb690123d0fbb3c705
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-29 04:34:44 +0530

    style(shell): add dynamic colors to oh-my-posh prompt

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 {
   home.packages = [
@@ -133,11 +138,33 @@
       };
     };
 
-    oh-my-posh = {
-      enable = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
-      configFile = ./oh-my-posh.toml;
-    };
+    oh-my-posh =
+      let
+        c = config.lib.stylix.colors;
+        toml = builtins.readFile ./oh-my-posh.toml;
+        themedToml =
+          builtins.replaceStrings
+            [
+              "@COLOR_FG@"
+              "@COLOR_BLUE@"
+              "@COLOR_GREEN@"
+              "@COLOR_YELLOW@"
+              "@COLOR_RED@"
+            ]
+            [
+              c.base05
+              c.base0D
+              c.base0B
+              c.base0A
+              c.base08
+            ]
+            toml;
+      in
+      {
+        enable = true;
+        enableZshIntegration = true;
+        enableBashIntegration = true;
+        configFile = pkgs.writeText "oh-my-posh.toml" themedToml;
+      };
   };
 }
