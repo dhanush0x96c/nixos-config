47f7d29d4173de47c49266e0d817ec68bfe9b54d
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-07-31 05:11:29 +0530

    feat(hyprland): add autostart applications

--- programs/hyprland/hypr/hyprland.lua
+++ programs/hyprland/hypr/hyprland.lua
@@ -22,16 +22,9 @@ local menu = "wofi"
 ---- AUTOSTART ----
 -------------------
 
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function()
+	hl.exec_cmd("waybar & mako & hyprpaper & hypridle")
+end)
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
