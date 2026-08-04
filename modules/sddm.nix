713b5454715a0d24a3b84779102ce20aa90551ca
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-04 13:52:36 +0530

    feat(sddm): add sddm display manager theme module

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,8 +1,46 @@
-{ ... }:
+{ pkgs, config, ... }:
 
+let
+  colors = config.lib.stylix.colors.withHashtag;
+  sddm-theme = pkgs.where-is-my-sddm-theme.override {
+    themeConfig = {
+      General = {
+        passwordCharacter = "*";
+        passwordMask = true;
+        passwordInputWidth = 0.5;
+        passwordInputBorderWidth = 0;
+        passwordInputCursorVisible = false;
+        passwordFontSize = 80;
+        passwordAllowEmpty = false;
+        cursorBlinkAnimation = true;
+        showSessionsByDefault = false;
+        sessionsFontSize = 24;
+        showUsersByDefault = false;
+        usersFontSize = 48;
+        showUserRealNameByDefault = true;
+        backgroundFillMode = "aspect";
+        blurRadius = 0;
+        hideCursor = true;
+        font = config.stylix.fonts.monospace.name;
+        helpFont = config.stylix.fonts.monospace.name;
+        helpFontSize = 18;
+        backgroundFill = colors.base00;
+        basicTextColor = colors.base05;
+        passwordCursorColor = colors.base05;
+        passwordInputBackground = colors.base00;
+        passwordTextColor = colors.base05;
+        wrongPasswordBorderColor = colors.base08;
+      };
+    };
+  };
+in
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "where_is_my_sddm_theme";
+    extraPackages = [ sddm-theme ];
   };
+
+  environment.systemPackages = [ sddm-theme ];
 }
