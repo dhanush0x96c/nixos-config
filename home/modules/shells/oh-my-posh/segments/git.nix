f5dc5f71c9e353edf3b9d92ed788de58a588abd7
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-09 12:20:23 +0530

    style(omp): polish git prompt segment appearance

--- home/modules/shells/oh-my-posh/segments/git.nix
+++ home/modules/shells/oh-my-posh/segments/git.nix
@@ -2,21 +2,26 @@
 {
   type = "git";
   style = "plain";
-  foreground = "#${c.base0B}";
   template = builtins.concatStringsSep "" [
-    "{{ if .Detached }} {{ trunc 7 .Commit.Sha }}{{ else }} {{ .HEAD }}{{ end }}"
-    "{{ if .BranchStatus }} {{ .BranchStatus }}{{ end }}"
-    "{{ if gt .StashCount 0 }} *{{ .StashCount }}{{ end }}"
-    "<#${c.base0A}>"
-    "{{ if .Staging.Changed }} [{{ .Staging.String }}]{{ end }}"
-    "{{ if .Working.Changed }} {{ .Working.String }}{{ end }}</>"
+    " "
+    "{{ if .Detached }}<#${c.base0E}>{{ trunc 7 .Commit.Sha }}</>"
+    "{{ else }}<#${c.base0B}>{{ .HEAD }}</>{{ end }}"
+    "{{ if .Rebase }}<#${c.base03}> • </><#${c.base0E}> REBASE</>{{ end }}"
+    "{{ if .Merge }}<#${c.base03}> • </><#${c.base0C}> MERGE</>{{ end }}"
+    "{{ if .CherryPick }}<#${c.base03}> • </><#${c.base0A}> CHERRY-PICK</>{{ end }}"
+    "{{ if .Revert }}<#${c.base03}> • </><#${c.base08}>↺ REVERT</>{{ end }}"
+    "{{ if gt .Ahead 0 }} <#${c.base0B}>⇡{{ .Ahead }}</>{{ end }}"
+    "{{ if gt .Behind 0 }} <#${c.base08}>⇣{{ .Behind }}</>{{ end }}"
+    "{{ if gt .StashCount 0 }}<#${c.base03}> • </><#${c.base0E}>󰏖 {{ .StashCount }}</>{{ end }}"
+    "{{ if gt .Working.Unmerged 0 }}<#${c.base03}> • </><#${c.base08}>✖ {{ .Working.Unmerged }} conflict(s)</>{{ end }}"
+    "{{ if .Staging.Changed }}<#${c.base03}> • </><#${c.base0B}> {{ .Staging.String }}</>{{ end }}"
+    "{{ if .Working.Changed }}<#${c.base03}> • </><#${c.base0A}>✎ {{ .Working.String }}</>{{ end }}"
   ];
   options = {
     fetch_status = true;
     fetch_upstream_icon = true;
-    branch_icon = " ";
+    branch_icon = " ";
     branch_identical_icon = "";
-    branch_gone_icon = "";
     disable_with_jj = true;
   };
 }
