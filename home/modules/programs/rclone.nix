a4eb7722e16cf7e72db15ca2bda80a0514b253d4
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-18 01:12:22 +0530

    feat(rclone): configure gdrive remote mount

--- home/modules/programs/rclone.nix
+++ home/modules/programs/rclone.nix
@@ -1,7 +1,29 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.rclone = {
     enable = true;
+
+    remotes = {
+      gdrive = {
+        config = {
+          type = "drive";
+          scope = "drive";
+        };
+
+        secrets = {
+          client_id = "${config.home.homeDirectory}/.config/rclone/client_id";
+          client_secret = "${config.home.homeDirectory}/.config/rclone/client_secret";
+          token = "${config.home.homeDirectory}/.config/rclone/token.json";
+        };
+
+        mounts = {
+          "" = {
+            enable = true;
+            mountPoint = "${config.home.homeDirectory}/gdrive";
+          };
+        };
+      };
+    };
   };
 }
