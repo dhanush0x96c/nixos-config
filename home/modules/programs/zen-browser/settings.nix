ff76ed7b3915c16e6f35192119534456940601a2
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-17 19:28:34 +0530

    feat(zen-browser): disable built-in password manager and autofill popups

--- home/modules/programs/zen-browser/settings.nix
+++ home/modules/programs/zen-browser/settings.nix
@@ -6,5 +6,14 @@
     "zen.view.sidebar-expanded" = false;
     "zen.tab-bar.expanded" = false;
     "zen.tabs.vertical.expand-on-hover" = true;
+
+    # Password Manager & Autofill
+    "signon.rememberSignons" = false;
+    "signon.autofillForms" = false;
+    "signon.generation.enabled" = false;
+    "signon.management.page.fileImport.enabled" = false;
+    "signon.suggestImport" = false;
+    "extensions.formautofill.addresses.enabled" = false;
+    "extensions.formautofill.creditCards.enabled" = false;
   };
 }
