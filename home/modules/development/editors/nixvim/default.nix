6bdd9baac4e973f87fbb98612e9fc744c672821e
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-24 17:41:37 +0530

    feat(nixvim): add tokyonight colorscheme module

--- home/modules/development/editors/nixvim/default.nix
+++ home/modules/development/editors/nixvim/default.nix
@@ -5,8 +5,14 @@
     inputs.nixvim.homeModules.nixvim
   ];
 
+  stylix.targets.nixvim.enable = false;
+
   programs.nixvim = {
     enable = true;
     nixpkgs.source = inputs.nixpkgs;
+
+    imports = [
+      ./config
+    ];
   };
 }
