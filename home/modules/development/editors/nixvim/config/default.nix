b5cc8a35d042d0a2e25eefe7b23dee24fbf5c6af
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-08-25 20:20:40 +0530

    feat(nixvim): add default vim options and leader keys

--- home/modules/development/editors/nixvim/config/default.nix
+++ home/modules/development/editors/nixvim/config/default.nix
@@ -3,5 +3,6 @@
 {
   imports = [
     ./colorscheme.nix
+    ./options.nix
   ];
 }
---
+++ home/modules/development/editors/nixvim/config/options.nix
@@ -0,0 +1,44 @@
+{ ... }:
+
+{
+  globals = {
+    mapleader = " ";
+    maplocalleader = "\\";
+  };
+
+  opts = {
+    # Line numbers
+    number = true;
+    relativenumber = true;
+
+    # Indentation
+    tabstop = 2;
+    shiftwidth = 2;
+    expandtab = true;
+    smartindent = true;
+
+    # Search settings
+    ignorecase = true;
+    smartcase = true;
+    incsearch = true;
+
+    # UI and layout settings
+    termguicolors = true;
+    signcolumn = "yes";
+    cursorline = true;
+    scrolloff = 4;
+    sidescrolloff = 8;
+    wrap = false;
+
+    # Window splitting
+    splitbelow = true;
+    splitright = true;
+
+    # System and behavior settings
+    clipboard = "unnamedplus";
+    undofile = true;
+    confirm = true;
+    updatetime = 200;
+    timeoutlen = 300;
+  };
+}
