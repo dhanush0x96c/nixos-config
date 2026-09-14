30af19b379d84863820cb04a7a370df966662dde
Dhanush Shetty <154754292+dhanush0x96c@users.noreply.github.com>
2026-09-14 19:01:56 +0530

    feat(nixvim): configure lsp diagnostic sign icons

--- home/modules/development/editors/nixvim/plugins/lsp/default.nix
+++ home/modules/development/editors/nixvim/plugins/lsp/default.nix
@@ -14,6 +14,14 @@
     underline = true;
     update_in_insert = false;
     severity_sort = true;
+    signs = {
+      text = {
+        "__rawKey__vim.diagnostic.severity.ERROR" = " ";
+        "__rawKey__vim.diagnostic.severity.WARN" = " ";
+        "__rawKey__vim.diagnostic.severity.INFO" = " ";
+        "__rawKey__vim.diagnostic.severity.HINT" = " ";
+      };
+    };
     virtual_text = {
       spacing = 4;
       prefix = "●";
